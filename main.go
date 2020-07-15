7b6700d0d8de58e5c2dfea0298b464e59bb83692
tsuchinaga <woocun@gmail.com>
2020-07-15 22:48:07 +0900

    ウィンドウを表示して文字列出力

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
 func main() {
-	println("こんにちわーるど")
+	a := app.New()
+
+	w := a.NewWindow("Fyne Learn")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello, World!")))
+
+	w.Resize(fyne.NewSize(256, 256))
+	w.ShowAndRun()
 }
