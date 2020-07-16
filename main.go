0c566084d90276b7f7dad57d720df1e4f8dd6e9a
tsuchinaga <woocun@gmail.com>
2020-07-16 11:47:00 +0900

    ページ遷移のような挙動を追加

--- main.go
+++ main.go
@@ -1,50 +1,62 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/layout"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 	"gitlab.com/tsuchinaga/go-fyne-learning/theme"
 )
 
+var (
+	w           fyne.Window
+	currentPage int
+)
+
 func main() {
 	a := app.New()
 	a.Settings().SetTheme(&theme.MisakiTheme{})
 
-	w := a.NewWindow("Fyne Learn")
-
-	// label and button
-	box1 := widget.NewVBox(
-		widget.NewLabel("Hello, World!"),
-		widget.NewLabel("こんにちわーるど！"),
-		widget.NewButton("終了", func() {
-			a.Quit()
-		}))
-
-	// entry
-	box2 := widget.NewVBox(
-		widget.NewForm(
-			widget.NewFormItem("ID", widget.NewEntry()),
-			widget.NewFormItem("PASS", widget.NewPasswordEntry()),
-			widget.NewFormItem("メモ", widget.NewMultiLineEntry())),
-		widget.NewButton("ログイン", func() {
-			println("pressed login button")
-		}))
-
-	// radio, check and select
-	radio := widget.NewRadio([]string{"OK", "NG"}, func(s string) { println(s) })
-	radio.Horizontal = true
-	box3 := widget.NewVBox(
-		radio,
-		widget.NewHBox(
-			widget.NewCheck("りんご", func(b bool) { println("りんご", b) }),
-			widget.NewCheck("みかん", func(b bool) { println("みかん", b) }),
-			widget.NewCheck("いちご", func(b bool) { println("いちご", b) }),
-		),
-		widget.NewSelect([]string{"東京", "大阪", "名古屋", "福岡", "札幌"}, func(s string) { println(s) }))
-
-	w.SetContent(widget.NewVBox(box1, box2, box3))
+	w = a.NewWindow("Fyne Learn")
+	defaultPage()
 
 	w.Resize(fyne.NewSize(512, 512))
 	w.ShowAndRun()
+}
+
+func NewPage(i int) fyne.CanvasObject {
+	moveButtonsBox := widget.NewHBox(
+		widget.NewButton("前のページ", prevPage),
+		layout.NewSpacer(),
+		widget.NewLabel(fmt.Sprintf("ページ%d", i+1)),
+		layout.NewSpacer(),
+		widget.NewButton("次のページ", nextPage),
+	)
+	moveButtonsBox.CreateRenderer()
+
+	page := widget.NewVBox()
+	page.Append(moveButtonsBox)
+	page.Append(widget.NewButton("最初のページ", defaultPage))
+	return page
+}
+
+func defaultPage() {
+	currentPage = 0
+	w.SetContent(NewPage(currentPage))
+}
+
+func prevPage() {
+	if currentPage < 1 {
+		return
+	}
+	currentPage--
+	w.SetContent(NewPage(currentPage))
+}
+
+func nextPage() {
+	currentPage++
+	w.SetContent(NewPage(currentPage))
 }
