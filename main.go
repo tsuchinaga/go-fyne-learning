ee9b1e2c3f13fe40beaa5f106029e7a369945dbe
tsuchinaga <woocun@gmail.com>
2020-07-16 15:58:49 +0900

    ダイアログ各種の動作確認

--- main.go
+++ main.go
@@ -1,62 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"errors"
+	"time"
 
 	"fyne.io/fyne/layout"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/widget"
 	"gitlab.com/tsuchinaga/go-fyne-learning/theme"
-)
-
-var (
-	w           fyne.Window
-	currentPage int
 )
 
 func main() {
 	a := app.New()
 	a.Settings().SetTheme(&theme.MisakiTheme{})
 
-	w = a.NewWindow("Fyne Learn")
-	defaultPage()
+	w := a.NewWindow("Fyne Learn")
+	w.SetContent(widget.NewVBox(
+		layout.NewSpacer(),
+		widget.NewButton("NewConfirm", func() {
+			cnf := dialog.NewConfirm("確認", "fyne どうや？", func(b bool) { println("callback", b) }, w)
+			cnf.SetDismissText("微妙")
+			cnf.SetConfirmText("ええでこれ")
+			cnf.Show()
+		}),
+		widget.NewButton("NewFileOpen", func() {
+			fileOpen := dialog.NewFileOpen(func(closer fyne.URIReadCloser, err error) {
+				if err != nil {
+					println(err)
+				}
+				if closer != nil {
+					defer closer.Close()
+					println(closer.Name())
+					bc := bufio.NewScanner(closer)
+					for bc.Scan() {
+						println(bc.Text())
+					}
+				}
+			}, w)
+			fileOpen.SetDismissText("ほないくで")
+			fileOpen.Show()
+		}),
+		widget.NewButton("NewError", func() {
+			er := dialog.NewError(errors.New("エラーでたわ"), w)
+			er.SetDismissText("まじかーい")
+			er.Show()
+		}),
+		widget.NewButton("NewInformation", func() {
+			info := dialog.NewInformation("おしらせ", "ちょっときいて", w)
+			info.SetDismissText("おけまる")
+			info.Show()
+		}),
+		widget.NewButton("NewProgressInfinite", func() {
+			pr := dialog.NewProgressInfinite("処理中", "がんばってまーす", w)
+			pr.Show()
+			go func() {
+				time.Sleep(3 * time.Second)
+				pr.Hide()
+			}()
+		})))
 
 	w.Resize(fyne.NewSize(512, 512))
 	w.ShowAndRun()
-}
-
-func NewPage(i int) fyne.CanvasObject {
-	moveButtonsBox := widget.NewHBox(
-		widget.NewButton("前のページ", prevPage),
-		layout.NewSpacer(),
-		widget.NewLabel(fmt.Sprintf("ページ%d", i+1)),
-		layout.NewSpacer(),
-		widget.NewButton("次のページ", nextPage),
-	)
-	moveButtonsBox.CreateRenderer()
-
-	page := widget.NewVBox()
-	page.Append(moveButtonsBox)
-	page.Append(widget.NewButton("最初のページ", defaultPage))
-	return page
-}
-
-func defaultPage() {
-	currentPage = 0
-	w.SetContent(NewPage(currentPage))
-}
-
-func prevPage() {
-	if currentPage < 1 {
-		return
-	}
-	currentPage--
-	w.SetContent(NewPage(currentPage))
-}
-
-func nextPage() {
-	currentPage++
-	w.SetContent(NewPage(currentPage))
 }
