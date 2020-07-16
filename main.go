9a16f0ac2759ee60b02561e59afebfd26c503a32
tsuchinaga <woocun@gmail.com>
2020-07-16 19:19:02 +0900

    複数ウィンドウ開くボタンを追加

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"errors"
-	"time"
-
-	"fyne.io/fyne/layout"
-
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 	"gitlab.com/tsuchinaga/go-fyne-learning/theme"
 )
@@ -18,50 +12,28 @@ func main() {
 	a := app.New()
 	a.Settings().SetTheme(&theme.MisakiTheme{})
 
+	subWindows := make([]fyne.Window, 0)
 	w := a.NewWindow("Fyne Learn")
 	w.SetContent(widget.NewVBox(
 		layout.NewSpacer(),
-		widget.NewButton("NewConfirm", func() {
-			cnf := dialog.NewConfirm("確認", "fyne どうや？", func(b bool) { println("callback", b) }, w)
-			cnf.SetDismissText("微妙")
-			cnf.SetConfirmText("ええでこれ")
-			cnf.Show()
+		widget.NewButton("新しいウィンドウ", func() {
+			sw := a.NewWindow("Sub Window")
+			sw.SetContent(widget.NewVBox(
+				layout.NewSpacer(),
+				widget.NewButton("とじる", func() {
+					sw.Close()
+				})))
+			sw.Show()
+			subWindows = append(subWindows, sw)
 		}),
-		widget.NewButton("NewFileOpen", func() {
-			fileOpen := dialog.NewFileOpen(func(closer fyne.URIReadCloser, err error) {
-				if err != nil {
-					println(err)
-				}
-				if closer != nil {
-					defer closer.Close()
-					println(closer.Name())
-					bc := bufio.NewScanner(closer)
-					for bc.Scan() {
-						println(bc.Text())
-					}
-				}
-			}, w)
-			fileOpen.SetDismissText("ほないくで")
-			fileOpen.Show()
+		layout.NewSpacer(),
+		widget.NewButton("とじる", func() {
+			for _, sw := range subWindows {
+				sw.Close()
+			}
+			w.Close()
 		}),
-		widget.NewButton("NewError", func() {
-			er := dialog.NewError(errors.New("エラーでたわ"), w)
-			er.SetDismissText("まじかーい")
-			er.Show()
-		}),
-		widget.NewButton("NewInformation", func() {
-			info := dialog.NewInformation("おしらせ", "ちょっときいて", w)
-			info.SetDismissText("おけまる")
-			info.Show()
-		}),
-		widget.NewButton("NewProgressInfinite", func() {
-			pr := dialog.NewProgressInfinite("処理中", "がんばってまーす", w)
-			pr.Show()
-			go func() {
-				time.Sleep(3 * time.Second)
-				pr.Hide()
-			}()
-		})))
+	))
 
 	w.Resize(fyne.NewSize(512, 512))
 	w.ShowAndRun()
